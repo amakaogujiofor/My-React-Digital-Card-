0d5eb7c2f42cc5c5354d94b53c9840396bed7bdd
amakaogujiofor <80483692+amakaogujiofor@users.noreply.github.com>
2021-11-18 18:02:21 +0100

    minor link adjustments

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,12 @@ export default function Header() {
       <img src="./amaka.hero.jpeg" alt="amaka-ogujiofor" className="hero" />
       <h1>Amaka Ogujiofor</h1>
       <h3 className="role">Frontend Developer</h3>
-      <a href="amakaogujiofor.netlify.app" className="web-link">
+      <a
+        href="https://amakaogujiofor.netlify.app"
+        target="_blank"
+        rel="noreferrer"
+        className="web-link"
+      >
         amakaogujiofor.netlify.app
       </a>
       <div className="email-container">

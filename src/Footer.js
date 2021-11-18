66b89012e659e19088e7218eb84e8244e484f2dd
amakaogujiofor <80483692+amakaogujiofor@users.noreply.github.com>
2021-11-18 17:58:44 +0100

    added link targets

--- src/Footer.js
+++ src/Footer.js
@@ -3,13 +3,28 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="footer">
-      <a href="https://www.linkedin.com/in/amakaogujiofor/" className="social">
+      <a
+        href="https://www.linkedin.com/in/amakaogujiofor/"
+        target="_blank"
+        rel="noreferrer"
+        className="social"
+      >
         <i className="fab fa-linkedin fa-2x"> </i>
       </a>
-      <a href="https://github.com/amakaogujiofor" className="social">
+      <a
+        href="https://github.com/amakaogujiofor"
+        target="_blank"
+        rel="noreferrer"
+        className="social"
+      >
         <i class="fab fa-github fa-2x"></i>
       </a>
-      <a href="https://www.twitter.com/amakaogujiofor" className="social">
+      <a
+        href="https://www.twitter.com/amakaogujiofor"
+        target="_blank"
+        rel="noreferrer"
+        className="social"
+      >
         <i class="fab fa-twitter-square  fa-2x"></i>
       </a>
     </div>
